d9e118baf237bdad2403739cc8b04828b121d091
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2025-04-27 19:01:27 -0500

    Add Person interface to define user attributes

---
+++ app/models/person.ts
@@ -0,0 +1,9 @@
+export interface Person {
+    nombres: string;
+    apellidoPaterno: string;
+    apellidoMaterno: string;
+    nombreCompleto: string;
+    tipoDocumento: string;
+    numeroDocumento: string;
+    digitoVerificador: string;
+}
