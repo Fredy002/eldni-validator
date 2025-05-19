78892f8d84db6ce4a4c7455e6d6fb2e095b2a38d
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2025-05-18 21:43:34 -0500

    Add EMPTY_DNI message to improve user feedback for empty DNI input

--- app/utils/messages.ts
+++ app/utils/messages.ts
@@ -2,4 +2,5 @@ export const Messages = {
     INVALID_LENGTH: 'Por favor ingresa un número de DNI de 8 dígitos.',
     NOT_FOUND: 'No se encontró ningún registro con ese DNI.',
     INVALID_DNI: 'Ingrese un número de DNI válido.',
+    EMPTY_DNI: "Por favor, ingresa el número de DNI."
 };
